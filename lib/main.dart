d5f3cd4f6bd79bf44bacaf47ac6ee189f8173b9c
david rojas <josedavidrojas323@gmail.com>
2025-07-13 22:40:36 -0500

    Auto commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:ejeapp/firebase_options.dart';
 import 'package:ejeapp/src/features/gif_grid/presentation/page/gif_grid_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ejeapp/src/injection/injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
